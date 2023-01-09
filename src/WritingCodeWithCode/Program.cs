85841d52f85388d734b5475639dc0db9fe600628
Steve Gordon <sgordon@hotmail.co.uk>
2023-01-09 13:26:00 +0000

    Finished demo

---
+++ src/WritingCodeWithCode/Program.cs
@@ -0,0 +1,53 @@
+﻿global using System.Text.Json;
+global using WritingCodeWithCode.Specification;
+global using Microsoft.CodeAnalysis.CSharp.Syntax;
+global using static Microsoft.CodeAnalysis.CSharp.SyntaxFactory;
+global using Microsoft.CodeAnalysis;
+global using Microsoft.CodeAnalysis.CSharp;
+
+var path = Directory.GetCurrentDirectory();
+
+Schema? schema = null;
+
+await using (var fileStream = File.OpenRead(Path.Combine(path, "spec.json")))
+{
+    schema = await JsonSerializer.DeserializeAsync<Schema>(fileStream, new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = true
+    });
+}
+
+if (schema is null)
+    throw new Exception("Unable to deserialise schema");
+
+var members = schema.Types.Select(t => CreateClass(t.TypeName, t.Properties)).ToArray()
+    ?? Array.Empty<MemberDeclarationSyntax>();
+
+var ns = FileScopedNamespaceDeclaration(IdentifierName("CodeGen")).AddMembers(members);
+
+await using var streamWriter = new StreamWriter(@"c:\code-gen\generated.cs", false);
+
+ns.NormalizeWhitespace()
+    .WriteTo(streamWriter);
+
+static ClassDeclarationSyntax CreateClass(string name, IEnumerable<SchemaProperty> properties)
+{
+	var classDeclaration = ClassDeclaration(Identifier(name))
+		.AddModifiers(Token(SyntaxKind.PublicKeyword));
+
+    foreach (var property in properties)
+    {
+        classDeclaration = classDeclaration.AddMembers(
+            PropertyDeclaration(
+                IdentifierName(property.Type),
+                Identifier(property.Name))
+            .AddModifiers(Token(SyntaxKind.PublicKeyword))
+            .AddAccessorListAccessors(
+                AccessorDeclaration(SyntaxKind.GetAccessorDeclaration)
+                    .WithSemicolonToken(Token(SyntaxKind.SemicolonToken)),
+                AccessorDeclaration(SyntaxKind.SetAccessorDeclaration)
+                    .WithSemicolonToken(Token(SyntaxKind.SemicolonToken))));
+    }
+
+    return classDeclaration;
+}
---
+++ src/WritingCodeWithCode/Specification/Schema.cs
@@ -0,0 +1,18 @@
+﻿namespace WritingCodeWithCode.Specification;
+
+public sealed class Schema
+{
+    public IReadOnlyCollection<SchemaTypes> Types { get; init; } = Array.Empty<SchemaTypes>();
+}
+
+public sealed class SchemaTypes
+{
+    public string TypeName { get; init; } = string.Empty;
+    public IReadOnlyCollection<SchemaProperty> Properties { get; init; } = Array.Empty<SchemaProperty>();
+}
+
+public sealed class SchemaProperty
+{
+    public string Type { get; init; } = string.Empty;
+    public string Name { get; init; } = string.Empty;
+}
