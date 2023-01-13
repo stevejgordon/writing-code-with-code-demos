8818f01a75ad5341bf5ec697c906dae6bcd3ea72
Steve Gordon <sgordon@hotmail.co.uk>
2023-01-13 10:52:54 +0000

    Add simple project for syntax visualizer demo

---
+++ src/VisualisingSyntax/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Examples;
+
+internal class Program
+{
+	private static void Main()
+	{
+		var data = new Data { Id = 1 };
+		Console.WriteLine(data.Id);
+	}
+}
+
+public class Data
+{
+	public required int Id { get; init; }
+}
