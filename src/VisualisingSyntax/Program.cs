48f4125aa29b6c823b4bf2f611ccf6f7e66093cb
Steve Gordon <sgordon@hotmail.co.uk>
2023-01-19 18:05:46 +0000

    Add comments for visualisation demo

--- src/VisualisingSyntax/Program.cs
+++ src/VisualisingSyntax/Program.cs
@@ -5,11 +5,14 @@ internal class Program
 	private static void Main()
 	{
 		var data = new Data { Id = 1 };
-		Console.WriteLine(data.Id);
-	}
+		Console.WriteLine(data.Id); // Write to console.
+	} 
 }
 
 public class Data
 {
+	/// <summary>
+	/// The ID.
+	/// </summary>
 	public required int Id { get; init; }
 }
